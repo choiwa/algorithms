7a485d0e5874be440ccc0bb31982606452aa7475
Joyce Cho <joyjoycho@gmail.com>
2019-01-10 20:33:17 -0800

    alternative solution for linearSearch

--- linearSearch.js
+++ linearSearch.js
@@ -6,6 +6,16 @@ function linearSearch(list, item) {
     }
 
     return false;
+
+    // alternative solution
+//     let index = -1 // false
+//     list.forEach((listItem, i) => {
+//         if (listItem === item) {
+//             index = i;
+//         }
+//     })
+
+//     return index;
 }
 
 linearSearch([2,6,7,90,103], 90)
