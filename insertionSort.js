d7e8dbca80d9fe2ae3724b4045ae5f32cc1bf67e
choiwa <joyjoycho@gmail.com>
2019-07-03 21:53:51 -0700

    alternative solution

--- insertionSort.js
+++ insertionSort.js
@@ -12,3 +12,20 @@ function insertionSort(nums) {
 let nums = [5, 3, 8, 2, 6, 4, 7, 9, 1, 10];
 
 insertionSort(nums);
+
+// alternative solution
+function insertionSort(nums) {
+  for (let i = 0; i < nums.length; i += 1) {
+    let curr = nums[i];
+
+    for (var j = i - 1; j >= 0 && nums[j] > curr; j--) {
+      nums[j + 1] = nums[j];
+    }
+    nums[j + 1] = curr;
+  }
+
+  return nums;
+}
+
+const nums = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+insertionSort(nums)
