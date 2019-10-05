a57332ab6315f2215d6bde90ff38fa8fefe7f519
choiwa <joyjoycho@gmail.com>
2019-10-05 16:55:11 -0700

    solution with hash

--- twoSum.js
+++ twoSum.js
@@ -1,13 +1,30 @@
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i += 1) {
-    const firstNum = nums[i];
+// solution with hash;
+const twoSum = function (nums, target) {
+  let hash = {};
 
-    for (let j = i + 1; j < nums.length; j += 1) {
-      const secondNum = nums[j];
+  for (let i = 0; i < nums.length; i++) {
+    let value = nums[i]
 
-      if (firstNum + secondNum === target) {
-        return [i, j];
-      }
+    let nextValue = target - value;
+
+    if (hash[nextValue] !== undefined) {
+      return [i, hash[nextValue]];
     }
+
+    hash[value] = i;
   }
-};
+}
+
+// var twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i += 1) {
+//         const firstNum = nums[i];
+
+//         for (let j = i + 1; j < nums.length; j += 1) {
+//             const secondNum = nums[j];
+
+//             if (firstNum + secondNum === target) {
+//                 return [i, j];
+//             }
+//         }
+//     }
+// };
