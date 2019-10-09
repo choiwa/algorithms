37bd4a5deeda55033a5aa3af69232e2dc9241820
choiwa <joyjoycho@gmail.com>
2019-10-09 15:04:12 -0700

    optimize solution

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -1,18 +1,35 @@
-var maxSubArray = function(nums) {
-    let output = undefined;
-    
-       for (let i = 0; i < nums.length; i += 1) {
-         let head = nums.slice(i);
-         let sum = 0;
+var maxSubArray = function (nums) {
+  let sum = 0;
+  let max = nums[0];
 
-         for (let j = 0; j < head.length; j += 1) {
-           sum += head[j];
+  for (let i = 0; i < nums.length; i += 1) {
+    let curr = nums[i];
+    sum += curr;
 
-           if (output === undefined || output < sum) {
-             output = sum;
-           }
-         }
-       }
+    if (max < sum) {
+      max = sum;
+    }
 
-       return output;
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+
+  return max;
+  // let output = undefined;
+
+  // for (let i = 0; i < nums.length; i += 1) {
+  //   let head = nums.slice(i);
+  //   let sum = 0;
+
+  //   for (let j = 0; j < head.length; j += 1) {
+  //     sum += head[j];
+
+  //     if (output === undefined || output < sum) {
+  //       output = sum;
+  //     }
+  //   }
+  // }
+
+  // return output;
 };
