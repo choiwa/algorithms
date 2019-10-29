2b423bf284012fda28975f4a23a5b6d46d5ed560
choiwa <joyjoycho@gmail.com>
2019-10-29 16:54:44 -0700

    constraints of generate parens

--- generateParentheses.js
+++ generateParentheses.js
@@ -7,10 +7,12 @@ var generateParenthesis = function (n) {
       return combinations.push(tempString);
     }
 
+    // continues to add open parens until it reaches max;
     if (openParen < n) {
       bfs(tempString + "(", openParen + 1, closeParen);
     }
 
+    // as long as we have enough open parens we can close it with no problem;
     if (openParen > closeParen) {
       bfs(tempString + ")", openParen, closeParen + 1);
     }
