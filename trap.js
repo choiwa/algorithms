4c6bc376d092a43f98eb05b5369a87c32918c5f1
Joyce Cho <joyjoycho@gmail.com>
2020-04-15 23:49:08 -0700

    leetcode 42

---
+++ trap.js
@@ -0,0 +1,12 @@
+var trap = function (height) {
+  let begin = height[0];
+  let end;
+  let count = 0;
+
+  for (let i = 0; i < height.length; i += 1) {
+    const curr = height[i];
+    const next = height[i + 1];
+  }
+
+  return count;
+};
