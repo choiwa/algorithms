658d7e50f55fc4fae0aef6ec02275cc61cc56025
Joyce Cho <joyjoycho@gmail.com>
2020-04-16 23:46:16 -0700

    edit solution

--- trap.js
+++ trap.js
@@ -1,12 +1,33 @@
-var trap = function (height) {
-  let begin = height[0];
+const trap = (height) => {
+  let begin = 0;
   let end;
   let count = 0;
 
-  for (let i = 0; i < height.length; i += 1) {
-    const curr = height[i];
-    const next = height[i + 1];
+  while (begin < height.length) {
+    end = begin + 1;
+
+    if (height[begin] > height[end]) {
+      let sum = 0;
+
+      for (let i = end; i < height.length; i += 1) {
+        if (height[i] >= height[begin]) {
+          count += sum;
+          begin = i;
+          break;
+        } else {
+          sum += height[begin] - height[end];
+        }
+
+        if (i === height.length - 1) {
+          begin += 1;
+        }
+      }
+    } else {
+      begin += 1;
+    }
   }
 
   return count;
 };
+
+trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
