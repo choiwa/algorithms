215dae0ce7ac972b187b4e914512aaa6e9f2d58a
Joyce Cho <joyjoycho@gmail.com>
2020-06-02 21:47:42 -0700

    clean code

--- rangeOfNumbers.js
+++ rangeOfNumbers.js
@@ -1,9 +1,8 @@
 function rangeOfNumbers(startNum, endNum) {
   if (startNum > endNum) {
     return [];
-  } else {
-    let currArr = rangeOfNumbers(startNum, endNum - 1);
-    currArr.push(endNum);
-    return currArr;
   }
+  let currArr = rangeOfNumbers(startNum, endNum - 1);
+  currArr.push(endNum);
+  return currArr;
 }
