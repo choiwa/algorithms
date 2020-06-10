cdea033d3e5a12c3d98a23d9538cce2cbffd4f7e
Joyce Cho <joyjoycho@gmail.com>
2020-06-10 16:17:33 -0700

    brute force solution

---
+++ missingLetters.js
@@ -0,0 +1,18 @@
+// function fearNotLetter(str) {
+//     const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+//     for (let i = 0; i < alphabet.length; i += 1) {
+//       if (str[0] === alphabet[i]){
+//         let count = 0;
+//         for (let j = i + 1; j < alphabet.length; j += 1) {
+//           count += 1;
+//           if (str[count] !== alphabet[j]) {
+//             return alphabet[j];
+//           }
+//         }
+//       }
+//     }
+//    return undefined;
+//   }
+
+//   fearNotLetter("abce");
